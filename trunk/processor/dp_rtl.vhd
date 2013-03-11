a1b73911cab16efca4c1bcdceb7509f228241584
peterbosc@gmail.com <peterbosc@gmail.com@821d776e-9c7b-97bd-ab1d-67d7798e35a7>
2013-03-11 22:50:18 +0000

    Added LR load/store instructions

--- trunk/processor/dp_rtl.vhd
+++ trunk/processor/dp_rtl.vhd
@@ -18,15 +18,16 @@ entity dp is
         lr_we,
         ir_we,
         ra_we,
-		  cr_we,
+	cr_we,
         addr_source,
         r_jump,
-		  lr_jump,
+	lr_jump,
         imm_enable,
         mux_b_enable,
         imm_signed,
         carry_enable,
-		  mem_write,
+	lr_rw,
+	mem_write,
         mem_read : in std_logic;
         data_bus : inout std_logic_vector(WIDTH-1 downto 0);
       --  data_bus : out std_logic_vector(WIDTH-1 downto 0);
@@ -60,7 +61,8 @@ architecture rtl of dp is
 
   signal cia, cia_next,
           nia, nia_next,
-          lr, lr_next : unsigned(WIDTH-1 downto 0);
+          lr, lr_next,
+	  lr_in : unsigned(WIDTH-1 downto 0);
          
   signal ir, cr, ir_next, cr_next_2, cr_next, cr_ca : std_logic_vector(WIDTH-1 downto 0);
   
@@ -71,48 +73,51 @@ architecture rtl of dp is
           imm_extended,
           imm_expanded : unsigned(WIDTH-1 downto 0);
   
-  signal cmp_eq, cmp_lt, cmp_gt, carry_in, carry_out, cr_we_2, ra_we_2 : std_logic;
+  signal cmp_eq, cmp_lt, cmp_gt, carry_in, carry_out, cr_we_2, ra_we_2, lr_we_2 : std_logic;
   
   signal ra_out,
-         rb_out,
-         rc_out,
-         ra_in,
-			cr_in,
-			ra_next,
-         mux_b,
-         mux_c,
-         imm_val,
-         alu_out
+          rb_out,
+          rc_out,
+          ra_in,
+	  cr_in,
+	  ra_next,
+	  mux_b,
+          mux_c,
+          imm_val,
+          alu_out
 : std_logic_vector(WIDTH-1 downto 0);
   
   signal ra,
-         rb,
-         rc : unsigned(WIDTH_REG_ADDR-1 downto 0);
+          rb,
+          rc : unsigned(WIDTH_REG_ADDR-1 downto 0);
   
   signal imm : std_logic_vector(15 downto 0);
+
+  signal ra_src : std_logic_vector(1 downto 0);
   
 begin
   
   alu_inst : palu port map(mux_a => aluop, a => mux_b, b => mux_c, c => alu_out, cmp_eq => cmp_eq, cmp_lt => cmp_lt, cmp_gt => cmp_gt, carry_in => carry_in, carry_out => carry_out);
   
-  nia_next <= (nia_base+nia_offset) when nia_we = '1' else nia; 
-  cia_next <= nia 						when cia_we = '1' else cia;   
-  lr_next  <= cia 						when lr_we = '1'	else lr;
-  ir_next  <= data_bus	 				when ir_we = '1'	else ir;  
-  ra_next  <= ra_in						when ra_we_2 = '1'	else ra_out;
-  cr_next_2 <= cr_in						when cr_we = '1'	else cr_ca;
-  cr_next  <= cr_next_2  					when cr_we_2 = '1'	else cr;
+  nia_next	<= (nia_base+nia_offset)			when nia_we = '1'	else nia; 
+  cia_next	<= nia 						when cia_we = '1'	else cia;   
+  lr_next	<= lr_in 					when lr_we_2 = '1'	else lr;
+  ir_next	<= data_bus	 				when ir_we = '1'	else ir;  
+  ra_next	<= ra_in					when ra_we_2 = '1'	else ra_out;
+  cr_next_2	<= cr_in					when cr_we = '1'	else cr_ca;
+  cr_next	<= cr_next_2  					when cr_we_2 = '1'	else cr;
+
+
   nia_base <= lr when lr_jump = '1' else cia;  
   nia_offset <= to_unsigned(1, WIDTH) when r_jump = '0' else unsigned(mux_c);
   
-  carry_in <= cr(3) and carry_enable;
-  
-  cr_we_2 <= execute and (carry_enable or cr_we);
-  
+  carry_in <= cr(3) and carry_enable;  
+  cr_we_2 <= execute and (carry_enable or cr_we);  
   ra_we_2 <= execute and ra_we;
-  
+  lr_we_2 <= (execute and lr_rw) or lr_we;
   cr_in <= x"0000000" & '0' & cmp_gt & cmp_eq & cmp_lt;
   cr_ca <= x"0000000" & carry_out & cr(2 downto 0); 
+
   ir_o <= ir;
   cr_o <= cr;
   
@@ -135,37 +140,38 @@ begin
   mux_b <= (others => '0') when ((rb = x"00000000") and mux_b_enable = '1') else rb_out;
   mux_c <= rc_out when imm_enable = '0' else std_logic_vector(imm_val);
   
-  ra_in <= data_bus when mem_read='1' else alu_out;
+  ra_src <= mem_read & lr_rw;
+
+  with ra_src select
+	ra_in <= data_bus 			when "10",
+	         alu_out			when "00",
+		 std_logic_vector(lr)  		when "01",
+		 alu_out  			when others; 
+  lr_in    <= unsigned(ra_out) when (lr_rw and execute) = '1' else cia;
   
   addr_bus <= std_logic_vector(nia) when addr_source='1' else alu_out;
   
   data_bus <= ra_out when mem_write = '1' else (others => 'Z');
   
   process(clk, reset)
-  begin
-    
+  begin    
     if (reset = '1')
     then
       cia <= (others => '0');
       nia <= (others => '0');
       lr  <= (others => '0');
       ir  <= (others => '0');
-      cr  <= (others => '0');
-      
-      reg <= (others => (others => '0'));
-        
+      cr  <= (others => '0');      
+      reg <= (others => (others => '0'));        
     elsif (rising_edge(clk))
-    then
-      
-      reg <= reg;
-      
-      cia <= cia_next;
-      nia <= nia_next;
-		lr  <= lr_next;
-		ir  <= ir_next;
-		cr  <= cr_next;
-      reg(to_integer(ra)) <= ra_next; 
-    
+    then      
+	reg <= reg;      
+	cia <= cia_next;
+	nia <= nia_next;
+	lr  <= lr_next;
+	ir  <= ir_next;
+	cr  <= cr_next;
+	reg(to_integer(ra)) <= ra_next;     
     end if;
     
     
--- trunk/processor/idec.vhd
+++ trunk/processor/idec.vhd
@@ -14,6 +14,7 @@ port(
   mux_b_enable,
   imm_signed,
   carry_enable,
+  lr_rw,
   cr_we,
   bus_rw,
   mem_read : out std_logic;
@@ -34,6 +35,7 @@ architecture idec_cl of idec is
 	ir_imm,
 	ir_shiftop,
 	ir_rw,
+	ir_lr,
 	ir_carry_enable,
 	ir_lr_jump,
 	ir_lr_store,
@@ -45,67 +47,64 @@ architecture idec_cl of idec is
 	signal ir_cond  : std_logic_vector(1 downto 0);
 	signal ir_width : std_logic_vector(1 downto 0);
 begin
-   ir_imm			<= ir(2);
+	ir_imm				<= ir(2);
 	
-	loadstore_op 	<= ir(0) nor ir(1);						--Type 0 (A NOR B == (NOT A) AND (NOT B))
-	logic_op 	   <= ir(0) and not ir(1);					--Type 1
-	arith_op 	   <= ir(1) and not ir(0);					--Type 2 
-	branch_op 	   <= ir(0) and ir(1);						--Type 3
+	loadstore_op 			<= ir(0) nor ir(1);					--Type 0 (A NOR B == (NOT A) AND (NOT B))
+	logic_op 	  		<= ir(0) and not ir(1);				--Type 1
+	arith_op 	   		<= ir(1) and not ir(0);				--Type 2 
+	branch_op 	   		<= ir(0) and ir(1);					--Type 3
 	
-	imm_enable		<= ir_imm;
-	imm_signed		<= branch_op or arith_op;				--Could be optimized to ir(1)
-	
+	imm_enable			<= ir_imm;
+	imm_signed			<= branch_op or arith_op;				--Could be optimized to ir(1)
 	ir_aluop			<= ir(5 downto 4);
-	
 	ra_we				<= logic_op or load_op or (arith_op and not compare_op);
-	
-	aluop(1 downto 0) <= ir_aluop;
-	aluop(2)				<= arith_op;
+	aluop(1 downto 0) 		<= ir_aluop;
+	aluop(2)			<= arith_op;
 	
 	--Start of load/store instruction decoding
 	
-	ir_rw				<= ir(3);									--~Read/Write
-	ir_width			<= ir(5 downto 4);
+	ir_rw				<= ir(3);						--~Read/Write
+	ir_lr				<= ir(4);
 	
-	mux_b_enable	<= loadstore_op and ir_imm;
-	mem_read			<= loadstore_op;							--It is not a problem if mem_read
-																		--is high during a write, it only
-																		--selects the writeback source.
+	mux_b_enable			<= loadstore_op and ir_imm;
+	mem_read			<= loadstore_op;					--It is not a problem if mem_read
+												--is high during a write, it only
+												--selects the writeback source.
+	lr_rw				<= ir_rw;
 																		
-	bus_rw			<= loadstore_op nand ir_rw;			--bus_rw is Read/~Write		
-	load_op			<= loadstore_op and not ir_rw;		--Read Ra when storing 		
+	bus_rw				<= (loadstore_op and not ir_lr) nand ir_rw;		--bus_rw is Read/~Write		
+	load_op				<= loadstore_op and not ir_rw;				--Read Ra when storing 		
 	
 	--TODO: Halfword and byte long load/stores OR CR/LR load stores
 	
 	--Start of logic instruction decoding
 	
-	ir_shiftop		<= ir(3);	
+	ir_shiftop			<= ir(3);	
 	
 	--Start of arithmethic instruction decoding	
 	
-	ir_carry_enable <= ir(3);
+	ir_carry_enable			<= ir(3);
 	
-	compare_op		<= ir_aluop(0) and ir_aluop(1);		--Compare doesnt store result	
+	compare_op			<= ir_aluop(0) and ir_aluop(1);			--Compare doesnt store result	
 	cr_we				<= arith_op and compare_op;
-	carry_enable <= ir_carry_enable;
-	--TODO: Carry handling
+	carry_enable 			<= ir_carry_enable;
 	
 	--Start of branch instruction decoding
 	
-	ir_lr_jump		<= ir(3);
-	ir_cond			<= ir(5 downto 4);
-	ir_lr_store 	<= ir(6);
+	ir_lr_jump			<= ir(3);
+	ir_cond				<= ir(5 downto 4);
+	ir_lr_store 			<= ir(6);
 	
-	store_lr			<= branch_op and ir_lr_store;			--Store CIA in LR
+	store_lr			<= branch_op and ir_lr_store;				--Store CIA in LR
 	
 	with ir_cond select
 		cond_val <= cr(0) when "00",							--Branch on LT
-						cr(1) when "01",							--Branch on BEQ
-						cr(2) when "10",							--Branch on BGT
-						'1'   when "11",							--Branch Always 
-						'-'	when others;
+			    cr(1) when "01",							--Branch on BEQ
+			    cr(2) when "10",							--Branch on BGT
+			    '1'   when "11",							--Branch Always 
+			    '-'	  when others;
 	
-	jump_i		<= branch_op	and		cond_val;
-	r_jump		<= jump_i		and not	ir_lr_jump;		--Relative Jump
-   lr_jump		<= jump_i		and		ir_lr_jump;		--Jump to LR
+	jump_i				<= branch_op		and		cond_val;
+	r_jump				<= jump_i		and not	ir_lr_jump;	--Relative Jump
+  	lr_jump				<= jump_i		and		ir_lr_jump;	--Jump to LR
 end idec_cl;
